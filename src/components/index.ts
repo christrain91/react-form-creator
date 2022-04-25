92a84ea91436ab9581f459ee722ad30984f4f4c1
Christopher Train <ctrain@pcs-publishing.com>
2022-04-25 11:20:04 +0100

    Export the individual forms

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as FormEditor } from './FormEditor'
 export { default as FormRenderer } from './FormRenderer'
 export { default as DefaultTools } from './Tools'
+export * from './Tools'
